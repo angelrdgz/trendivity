5731098af9601706d327625946b6d9ec59b73bcc
angelrodgz <angelrodriguez@ucol.mx>
2019-10-17 09:18:50 -0500

    secure assets

---
+++ resources/views/emails/contact.blade.php
@@ -0,0 +1,6 @@
+<body>
+    <p>Un usuario esta interesado en la plataforma, te dejamos su información para que te comuniques con el.</p>
+    <p><b>Nombre:</b> {{$name}}</p>
+    <p><b>Email:</b> {{$email}}</p>
+    <p><b>Mensaje:</b> {{$mess}}</p>
+</body>
